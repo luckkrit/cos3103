5bbb2428cf6cc85a07abdddc8a6cdf8953d0efd8
Krit Chomaitong <luckkrit@hotmail.com>
2025-08-13 13:36:49 +0700

    update slides

---
+++ public/slides/sql_storedproc_views/assets/employee-Dpx05jnF.js
@@ -0,0 +1,1 @@
+const s="/cos3103/slides/sql_storedproc_views/images/sql-storedproc-views/employee.png";export{s as _};
---
+++ public/slides/sql_storedproc_views/assets/three-schema-architecture-B_rt2zvt.js
@@ -0,0 +1,1 @@
+const s="/cos3103/slides/sql_storedproc_views/images/sql-storedproc-views/three-schema-architecture.png";export{s as _};
