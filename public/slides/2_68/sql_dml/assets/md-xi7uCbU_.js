1e57700631b14dab87ee49366893dedc6887f63c
Krit Chomaitong <luckkrit@hotmail.com>
2025-12-23 22:43:57 +0700

    update sql_dml

---
+++ public/slides/2_68/sql_dml/assets/md-xi7uCbU_.js
@@ -0,0 +1,14 @@
+import{_ as h}from"./CsvTable-BKntFO2z.js";import{_ as m}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BzSd8VN2.js";import{b as d,o as c,w as t,i as E,e as a,g as s,m as g,C as l,v as u,x as f,B as n}from"./modules/vue-Bkd3V_B_.js";import{I as _}from"./two-cols-title-rrTwyMBs.js";import{u as C,f as y}from"./slidev/context-BpFsYB5C.js";import"./modules/unplugin-icons-XtNCj0Jw.js";import"./index-BgrtNpFu.js";import"./modules/shiki-Bi5MuodP.js";const I={__name:"sql_dml.md__slidev_27",setup(F){const{$clicksContext:r,$frontmatter:o}=C();return r.setup(),(B,i)=>{const k=m,p=h;return c(),d(_,u(f(n(y)(n(o),26))),{title:t(e=>[...i[0]||(i[0]=[s("p",null,[s("span",null,"Boolean Expression in SELECT-Clause")],-1)])]),left:t(e=>[a(k,g({},{title:"",ranges:[]}),{default:t(()=>[...i[1]||(i[1]=[s("pre",{class:"shiki shiki-themes slack-dark snazzy-light slidev-code",style:{"--shiki-dark":"#E6E6E6","--shiki-light":"#565869","--shiki-dark-bg":"#222222","--shiki-light-bg":"#FAFBFC"}},[s("code",{class:"language-sql"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#569CD6","--shiki-light":"#11658F"}},"SELECT"),s("span",{style:{"--shiki-dark":"#E6E6E6","--shiki-light":"#565869"}}," creditLimit , creditLimit "),s("span",{style:{"--shiki-dark":"#D4D4D4","--shiki-light":"#ADB1C2"}},">"),s("span",{style:{"--shiki-dark":"#B5CEA8","--shiki-light":"#FF5C57"}}," 100000")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#569CD6","--shiki-light":"#11658F"}},"FROM"),s("span",{style:{"--shiki-dark":"#E6E6E6","--shiki-light":"#2DAE58"}}," classicmodels"),s("span",{style:{"--shiki-dark":"#E6E6E6","--shiki-light":"#565869"}},"."),s("span",{style:{"--shiki-dark":"#E6E6E6","--shiki-light":"#2DAE58"}},"customers")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#569CD6","--shiki-light":"#11658F"}},"LIMIT"),s("span",{style:{"--shiki-dark":"#B5CEA8","--shiki-light":"#FF5C57"}}," 10")])])],-1)])]),_:1},16)]),right:t(e=>[E(" ![SELECT boolean](/images/2_68/sql_dml/select_boolean.png) "),a(p,null,{default:t(()=>[...i[2]||(i[2]=[s("pre",null,`"creditlimit"	"?column?"
+21000	false
+71800	false
+117300	true
+118200	true
+81700	false
+210500	true
+0	false
+59700	false
+64600	false
+114900	true
+`,-1)])]),_:1})]),_:1},16)}}};export{I as default};
